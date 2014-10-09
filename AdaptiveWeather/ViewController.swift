9b07f0e1415526572d7bc49203c01d3c200055e1
robertlyc <merlinlyc@gmail.com>
2014-10-09 12:55:03 +0800

    add constraint

---
+++ AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by RoBeRt on 14-10-9.
+//  Copyright (c) 2014年 RoBeRt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
